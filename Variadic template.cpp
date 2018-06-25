c5ef551e4c2d9f15edc17d62172667ff4c8a738e
avovana <vladimir.englishbox@gmail.com>
2018-06-25 22:17:42 +0300

    Update Variadic template.cpp

--- Variadic template.cpp
+++ Variadic template.cpp
@@ -175,3 +175,83 @@ int main()
     doSomethingForAll(1,2,3,4);
     return 0;
 }
+
+// Iterating over parametr pack with if constexpr C++17 and print
+/*
+if constexpr
+*/
+
+#include <iostream>
+
+template <typename T>
+void print(T value) 
+{
+	std::cout << value << std::endl;
+}
+
+template <int number, typename Head,  typename... Rest>
+void get(Head firstValue, Rest... rest)
+{
+    if constexpr(number > 0) 
+    {
+        std::cout << "number > 0" << '\n';
+        get<number - 1>(rest...);
+    } 
+    else 
+    {
+        std::cout << "number = 0" << '\n';
+        print(firstValue);
+    }
+}
+
+template <int number, typename... Rest>
+void print(Rest... rest)
+{
+    std::cout << "Parametr pack size: " << sizeof...(rest) << std::endl;
+    get<number>(rest...);
+}
+
+int main()
+{
+    print<2>(3, 4, 5.6);
+    return 0;
+}
+
+// Iterating over parametr pack with if constexpr C++17 getting value
+/*
+if constexpr
+*/
+
+#include <iostream>
+#include <typeinfo>
+
+template <int number, typename Head,  typename... Rest>
+auto get_impl(Head firstValue, Rest... rest)
+{
+    if constexpr(number > 0) 
+    {
+        std::cout << "number > 0" << '\n';
+        return get_impl<number - 1>(rest...);
+    } 
+    else 
+    {
+        std::cout << "number = 0" << '\n';
+        return firstValue;
+    }
+}
+
+template <int number, typename... Rest>
+auto get(Rest... rest)
+{
+    std::cout << "Parametr pack size: " << sizeof...(rest) << std::endl;
+    return get_impl<number>(rest...);
+}
+
+int main()
+{
+    auto value = get<2>(3, 4, 5.6);
+    std::cout << "value: " << value << '\n';
+    std::cout << "value has type: " << typeid(value).name() << '\n';
+        
+    return 0;
+}
