46db38c6498ee340010cfb04f4747a3990376aaa
avovana <vladimir.englishbox@gmail.com>
2018-06-27 11:41:55 +0400

    Update Pack expansion.cpp

--- Pack expansion.cpp
+++ Pack expansion.cpp
@@ -5,6 +5,10 @@ Description:
 Transmit arguments with different types to function thanks to tuple.
 Expand set of arguments in a initializer_list context.
 
+Extra:
+printExpansionPack function template for printing set of arguments.
+printRecursive function template for printing every element.
+
 Articles:
 https://arne-mertz.de/2016/11/more-variadic-templates/
 http://riptutorial.com/cplusplus/example/26687/turn-a-std--tuple-t-----into-function-parameters
@@ -17,17 +21,45 @@ http://riptutorial.com/cplusplus/example/26687/turn-a-std--tuple-t-----into-func
 #include <utility>
 
 template <class Arg>
-int doSomething(const Arg& arg)
+int printElement(const Arg& arg)
 {
     std::cout << "arg: " << arg << " ";
     return 1;
 }
 
-// ----------------------------------------------------------------------------
+//----------------------------------------------------------------------------
+template <typename T>
+void printRecursive(T arg)
+{
+    std::cout << "arg: " << arg << " ";
+}
+
+
+template <typename Head, typename... Tail>
+void printRecursive(Head head, Tail... tail)
+{
+    printRecursive(head);
+    printRecursive(tail...);
+    std::cout << '\n';
+}
+
+//----------------------------------------------------------------------------
+template <typename... Args>
+void printExpansionPack(Args... args)
+{
+    std::initializer_list<bool>{ (std::cout << args << ' ')... };
+}
+
+//----------------------------------------------------------------------------
+
 template <typename Tuple, std::size_t... I>
 void process(Tuple const& tuple, std::index_sequence<I...>) 
 {
-    auto x = {(doSomething(std::get<I>(tuple)))...};
+    //printRecursive(std::get<I>(tuple)...);
+    
+    //printExpansionPack(std::get<I>(tuple)...);
+    
+    auto x = {(printElement(std::get<I>(tuple)))...};
     std::cout << ". Size: " << x.size() << "\n" << "\n";
 }
 
@@ -37,7 +69,8 @@ void process(Tuple const& tuple)
     process(tuple, std::make_index_sequence<std::tuple_size<Tuple>::value>());
 }
 
-// ----------------------------------------------------------------------------
+//----------------------------------------------------------------------------
+
 int main() 
 {
     process(std::make_tuple(1, 3.14, std::string("foo")));
